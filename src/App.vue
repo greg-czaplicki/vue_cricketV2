b047bd24f59aa07f290a731d572b55da6da2cacd
glc3344 <glc3344@gmail.com>
2018-01-29 13:41:03 -0500

    Feat: Basic project setup with vuex.

---
+++ src/App.vue
@@ -0,0 +1,58 @@
+<template>
+  <div id="app">
+    <h1 id="title">{{ $store.state.title }}</h1>
+    <player-names></player-names>
+  </div>
+</template>
+
+<script>
+import PlayerNames from './components/PlayerNames';
+
+export default {
+  components: {
+    PlayerNames: PlayerNames
+  },
+  name: 'app',
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  padding-top: 10px;
+  color: #2c3e50;
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-auto-rows: 80px;
+}
+
+#title {
+  justify-self: center;
+  align-self: end;
+  margin: 0;
+}
+
+h1,
+h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/PlayerNames.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="container">
+    <h1 id="player1">Player 1</h1>
+    <h2 id="vs">VS.</h2>
+    <h1 id="player2">Player 2</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style lang="scss">
+#container {
+  display: grid;
+  grid-template-columns: 1fr 2fr 1fr 2fr 1fr;
+  justify-items: center;
+}
+
+#player1 {
+  grid-column: 2;
+}
+
+#vs {
+  align-self: center;
+}
+</style>
