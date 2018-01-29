4d1fcbcd4b1624e8ed5cb9c731dacd625d2b648e
glc3344 <glc3344@gmail.com>
2018-01-29 15:46:52 -0500

    Feat: Created toggle function to switch between active player along with css conditional styling

--- src/components/PlayerNames.vue
+++ src/components/PlayerNames.vue
@@ -1,31 +1,58 @@
 <template>
   <div id="container">
-    <h1 id="player1">Player 1</h1>
+    <h1 id="player1" :class="{isActive: $store.state.player1.isActive}" @click="togglePlayer1">
+      {{ $store.state.player1.name }}
+    </h1>
     <h2 id="vs">VS.</h2>
-    <h1 id="player2">Player 2</h1>
+    <h1 id="player2" :class="{isActive: $store.state.player2.isActive}" @click="togglePlayer2">
+      {{ $store.state.player2.name }}
+    </h1>
   </div>
 </template>
 
 <script>
+import { mapActions } from 'vuex';
+
 export default {
   data() {
     return {};
+  },
+  methods: {
+    ...mapActions(['togglePlayer1', 'togglePlayer2'])
   }
 };
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 #container {
   display: grid;
   grid-template-columns: 1fr 2fr 1fr 2fr 1fr;
-  justify-items: center;
 }
 
 #player1 {
   grid-column: 2;
+  padding: 22px 0;
+  margin: 0;
+  text-align: center;
+  align-self: center;
+}
+
+#player2 {
+  padding: 22px 0;
+  margin: 0;
+  text-align: center;
+  align-self: center;
 }
 
 #vs {
+  text-align: center;
+  margin: 0;
   align-self: center;
+}
+
+.isActive {
+  background-color: #baffe0;
+  border: 1px solid rgb(91, 182, 131);
+  border-radius: 3px;
 }
 </style>
--- src/store/store.js
+++ src/store/store.js
@@ -5,6 +5,61 @@ Vue.use(Vuex);
 
 export const store = new Vuex.Store({
   state: {
-    title: 'Cricket Scorer'
+    title: 'Cricket Scorer',
+    gameInProgess: true,
+    player1: {
+      name: 'Player_1',
+      isActive: true,
+      roundScore: 0,
+      totalScore: 0,
+      totalDartsThrown: 0,
+      currentDartsThrown: 0,
+      pointsPerMark: 0,
+      twenty: 0,
+      nineteen: 0,
+      eighteen: 0,
+      seventeen: 0,
+      sixteen: 0,
+      fifteen: 0,
+      bull: 0
+    },
+    player2: {
+      name: 'Player_2',
+      isActive: false,
+      roundScore: 0,
+      totalScore: 0,
+      totalDartsThrown: 0,
+      currentDartsThrown: 0,
+      pointsPerMark: 0,
+      twenty: 0,
+      nineteen: 0,
+      eighteen: 0,
+      seventeen: 0,
+      sixteen: 0,
+      fifteen: 0,
+      bull: 0
+    }
+  },
+  mutations: {
+    togglePlayer1(state) {
+      if (state.player1.isActive === false) {
+        state.player1.isActive = true;
+        state.player2.isActive = false;
+      }
+    },
+    togglePlayer2(state) {
+      if (state.player2.isActive === false) {
+        state.player2.isActive = true;
+        state.player1.isActive = false;
+      }
+    }
+  },
+  actions: {
+    togglePlayer1(context) {
+      context.commit('togglePlayer1');
+    },
+    togglePlayer2(context) {
+      context.commit('togglePlayer2');
+    }
   }
 });
