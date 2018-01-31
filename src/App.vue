65f97ef769c5925422b08f92b7f58e41f108fb2f
glc3344 <glc3344@gmail.com>
2018-01-30 20:21:41 -0500

    feat: rearranged grid to fit redo/undo at the top and added vuetify styling

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,19 @@
 <template>
-  <div id="app">
-    <h2 @click="undo">undo</h2>
+<div>
+  <div id="appHeader">
+    <v-btn fab dark medium color="red accent-2" class="fab-button" @click="undo">
+      <v-icon dark>undo</v-icon>
+    </v-btn>
     <h1 id="title">{{ $store.state.title }}</h1>
+    <v-btn fab dark medium color="green accent-2" class="fab-button" @click="redo">
+      <v-icon dark>redo</v-icon>
+    </v-btn>
+  </div>
+  <div class="scoreWrapper">
     <player-names></player-names>
     <segment></segment>
   </div>
+</div>
 </template>
 
 <script>
@@ -28,23 +37,33 @@ export default {
 <style lang="scss">
 body {
   padding: 10px;
-}
-
-#app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  padding-top: 10px;
   color: #2c3e50;
+}
+
+#appHeader {
+  display: grid;
+  grid-template-columns: 1fr 2fr 1fr;
+  grid-template-rows: 70px;
+  padding-bottom: 10px;
+}
+#scoreWrapper {
   display: grid;
   grid-template-columns: 1fr;
-  grid-template-rows: 40px 40px;
-  grid-auto-rows: 80px;
+  grid-template-rows: 40px;
+  // grid-auto-rows: 60px;
 }
 
 #title {
-  justify-self: center;
+  text-align: center;
+  align-self: center;
   margin: 0;
+}
+
+.fab-button {
+  justify-self: center;
 }
 
 h1,
--- src/main.js
+++ src/main.js
@@ -2,8 +2,10 @@ import Vue from 'vue';
 import App from './App.vue';
 import { store } from './store/store';
 import VuexUndoRedo from './plugins/plugins';
+import Vuetify from 'vuetify';
 
 Vue.use(VuexUndoRedo);
+Vue.use(Vuetify);
 
 new Vue({
   store: store,
